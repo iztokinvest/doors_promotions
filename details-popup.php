268e17467d14915fc4557d1aef8cde99b7afe018
iztokinvest <90605919+iztokinvest@users.noreply.github.com>
2024-06-04 15:48:26 +0300

    1.7.1

--- details-popup.php
+++ details-popup.php
@@ -1,7 +1,6 @@
 <?php
 // Include WordPress functions
-require_once('../../../wp-load.php'); // Adjust the path if necessary
-
+require_once(dirname(__FILE__, 4) . '/wp-load.php'); // Adjust the path if necessary
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,8 +8,7 @@ require_once('../../../wp-load.php'); // Adjust the path if necessary
 <head>
 	<meta charset="UTF-8">
 	<title><?php _e('Още детайли', 'your-plugin-textdomain'); ?></title>
-	<?php wp_head();
-	?>
+	<?php wp_head(); ?>
 </head>
 
 <body>
@@ -18,13 +16,12 @@ require_once('../../../wp-load.php'); // Adjust the path if necessary
 		<h1><?php _e('Информация', 'your-plugin-textdomain'); ?></h1>
 		<p>
 			<?php
-			_e('<b>URL за изчистване на кеша:</b><br>');
-			echo getcwd() . DIRECTORY_SEPARATOR . 'clear_cache.php';
+			_e('<b>URL за изчистване на кеша:</b><br>', 'your-plugin-textdomain');
+			echo esc_html(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'clear_cache.php');
 			?>
 		</p>
 	</div>
-	<?php wp_footer();
-	?>
+	<?php wp_footer(); ?>
 </body>
 
 </html>
