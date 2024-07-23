1d7bd5f3ab90c3a7ef2a6e79174d53c411780993
iztokinvest <90605919+iztokinvest@users.noreply.github.com>
2024-07-23 11:03:32 +0300

    1.10.2

--- assets/js/countdown_timer.js
+++ assets/js/countdown_timer.js
@@ -38,38 +38,38 @@ document.addEventListener("DOMContentLoaded", function () {
 			if (daysElement) {
 				daysElement.innerHTML =
 					days > 0
-						? `<span id="days-number">${days}</span><span id="days-title">${getPluralForm(
+						? `<span class="days-number">${days}</span><span id="days-title">${getPluralForm(
 								days,
-								" <span id='days-title'>ден</span>",
-								" <span id='days-title'>дни</span>"
+								" <span class='days-title'>ден</span>",
+								" <span class='days-title'>дни</span>"
 						  )}</span>`
 						: "";
 			}
 			if (hoursElement) {
 				hoursElement.innerHTML =
 					hours > 0
-						? `<span id="hours-number">${hours}</span><span id="hours-title">${getPluralForm(
+						? `<span class="hours-number">${hours}</span><span id="hours-title">${getPluralForm(
 								hours,
-								" <span id='hours-title'>час</span>",
-								" <span id='hours-title'>часа</span>"
+								" <span class='hours-title'>час</span>",
+								" <span class='hours-title'>часа</span>"
 						  )}</span>`
 						: "";
 			}
 			if (minutesElement) {
 				minutesElement.innerHTML =
 					minutes > 0
-						? `<span id="minutes-number">${minutes}</span><span id="minutes-title">${getPluralForm(
+						? `<span class="minutes-number">${minutes}</span><span id="minutes-title">${getPluralForm(
 								minutes,
-								" <span id='minutes-title'>минута</span>",
-								" <span id='minutes-title'>минути</span>"
+								" <span class='minutes-title'>минута</span>",
+								" <span class='minutes-title'>минути</span>"
 						  )}</span>`
 						: "";
 			}
 			if (secondsElement) {
-				secondsElement.innerHTML = `<span id="seconds-number">${seconds}</span><span id="seconds-title">${getPluralForm(
+				secondsElement.innerHTML = `<span class="seconds-number">${seconds}</span><span id="seconds-title">${getPluralForm(
 					seconds,
-					" <span id='seconds-title'>секунда</span>",
-					" <span id='seconds-title'>секунди</span>"
+					" <span class='seconds-title'>секунда</span>",
+					" <span class='seconds-title'>секунди</span>"
 				)}</span>`;
 			}
 
